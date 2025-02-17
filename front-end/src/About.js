a2cb434d1d4a4b31dffe26223850bf381aea73b6
mkphung29 <mkp6075@nyu.edu>
2025-02-17 16:37:59 -0500

    Create about page

---
+++ front-end/src/About.js
@@ -0,0 +1,12 @@
+import './About.css'
+
+const About = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+// make this component available to be imported into any other file
+export default About
