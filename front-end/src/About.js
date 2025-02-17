a5703ac1135f79d763562d0035eb8d46d0692d94
mkphung29 <mkp6075@nyu.edu>
2025-02-17 16:47:17 -0500

    Create nav link

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -16,7 +16,7 @@ const About = () => {
 
   return (
     <div className="about-container" style={{ maxWidth: '600px', margin: '0 auto', textAlign: 'center' }}>
-      <h1>About Me</h1>
+      <h1>About Us</h1>
       {aboutData.bio.map((paragraph, index) => (
         <p key={index} style={{ textAlign: 'left' }}>{paragraph}</p>
       ))}
